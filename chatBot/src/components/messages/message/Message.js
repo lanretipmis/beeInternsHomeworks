0ee92686988e561048b7eff384f4074b5bd1dc21
Andrew Smodlev <an.smodlev@gmail.com>
2020-04-22 19:01:52 +0300

    add folder

---
+++ chatBot/src/components/messages/message/Message.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./src/Message.css";
+import BotAvatar from "./src/BotAvatar.png";
+import ClientAvatar from "./src/ClientAvatar.png";
+import Skycons from "react-skycons";
+
+const Message = ({ msg }) => {
+  const { text, type } = msg;
+
+  return (
+    <div className="message_container">
+      <img
+        src={type === "client" ? ClientAvatar : BotAvatar}
+        className="message_avatar"
+        alt="avatar"
+      />
+      <div
+        className="message_text"
+        style={
+          type === "client"
+            ? { background: "#F9C35B" }
+            : { background: "#F1F0F0" }
+        }
+      >
+        <p>{text}</p>
+        {msg.icon && (
+          <Skycons
+            className="skycons"
+            color="black"
+            icon={msg.icon.toUpperCase().replace("-", "_")}
+            autoplay={true}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Message;
---
+++ chatBot/src/context/message/messageReducer.js
@@ -0,0 +1,130 @@
+import {
+  START,
+  STOP,
+  CLIENT_MSG,
+  SET_NUMS,
+  CALCULATED,
+  NOT_NUMS,
+  GREETING,
+  WRONG_COMMAND,
+  FORECAST,
+  FALSE_START
+} from "../types";
+import uuid from "uuid";
+
+export default (state, action) => {
+  switch (action.type) {
+    case FORECAST:
+      return {
+        ...state,
+        messages: [
+          ...state.messages,
+          {
+            text: `${action.payload.summary}`,
+            icon: action.payload.icon.replace('-','_'),
+            type: "bot",
+            id: uuid.v4()
+          }
+        ]
+      };
+    case SET_NUMS:
+      return {
+        ...state,
+        numbers: action.payload,
+        messages: [
+          ...state.messages,
+          {
+            text: "Введите одно из следующих действий:  -, +, *, /",
+            type: "bot",
+            id: uuid.v4()
+          }
+        ]
+      };
+    case NOT_NUMS:
+      return {
+        ...state,
+        messages: [
+          ...state.messages,
+          { text: "Введите числа, пожалуйста", type: "bot", id: uuid.v4() }
+        ]
+      };
+    case CALCULATED:
+      return {
+        ...state,
+        messages: [
+          ...state.messages,
+          { text: action.payload, type: "bot", id: uuid.v4() }
+        ]
+      };
+    case START:
+      return {
+        ...state,
+        started: true,
+        messages: [
+          ...state.messages,
+          {
+            text: "Привет, меня зовут Чат-бот, а как зовут тебя?",
+            type: "bot",
+            id: uuid.v4()
+          }
+        ]
+      };
+    case FALSE_START: {
+      return {
+        ...state,
+        messages: [
+          ...state.messages,
+          {
+            text: "Введите команду /start, для начала общения",
+            type: "bot",
+            id: uuid.v4()
+          }
+        ]
+      }
+      }
+    case GREETING:
+      return {
+        ...state,
+        messages: [
+          ...state.messages,
+          {
+            text: `Привет ${action.payload}, приятно познакомится. Я умею считать, введи числа которые надо посчитать`,
+            type: "bot",
+            id: uuid.v4()
+          }
+        ]
+      };
+    case STOP:
+      return {
+        ...state,
+        started: false,
+        messages: [
+          ...state.messages,
+          {
+            text: "Всего доброго, если хочешь поговорить пиши /start",
+            type: "bot",
+            id: uuid.v4()
+          }
+        ]
+      };
+    case WRONG_COMMAND:
+      return {
+        ...state,
+        messages: [
+          ...state.messages,
+          {
+            text: "Я не понимаю, введите другую команду!",
+            type: "bot",
+            id: uuid.v4()
+          }
+        ]
+      };
+    case CLIENT_MSG:
+      return {
+        ...state,
+        messages: [...state.messages, action.payload]
+      };
+    default:
+      return state;
+  }
+};
