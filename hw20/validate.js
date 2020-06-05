88a3394080334cefcebf5f430387885d3cdd2b38
Andrew Smodlev <an.smodlev@gmail.com>
2020-06-05 20:41:35 +0300

    done

--- hw20/validate.js
+++ hw20/validate.js
@@ -1,7 +1,11 @@
 function validate(data) {
-    const { login, password, confirmPassword, license } = data;
-
-    if (!login || !password) {
+    const { login, password, confirmPassword, license, firstName, gender } = data;
+    const wrongLogins = ['beeline', 'beeinterns', 'bee'];
+    if (!firstName) {
+        alert('Введите имя');
+    } else if (wrongLogins.some(wrondlogin => wrondlogin === login)) {
+        alert('Логин занят');
+    } else if (!login || !password) {
         alert('Укажите логин/пароль');
     } else if (password.length < 6) {
         alert('Пароль должен быть длинной не менее 6 символов');
@@ -10,6 +14,10 @@ function validate(data) {
     } else if (!license) {
         alert('Необходимо согласие');
     } else {
-        alert('Форма отправлена');
+        if (gender === "female") {
+            alert(`Уважаемая ${firstName}, заявка создана`);
+        } else {
+            alert(`Уважаемый ${firstName}, заявка создана`);
+        }
     }
 }
